80df079d97fdfb6cd330f82fc02992a8e8a0da99
Fayaz Wagid Hosain <f.r.wagidhosain@student.tudelft.nl>
2024-02-25 16:42:18 +0100

    Added comments

--- server/src/main/java/server/api/PersonController.java
+++ server/src/main/java/server/api/PersonController.java
@@ -9,19 +9,35 @@ import commons.Person;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Basic REST API for Person
+ */
 @RestController
 public class PersonController {
     private final PersonRepository repo;
 
+    /**
+     * Constructor for controller
+     * @param repo Person repository
+     */
     public PersonController(PersonRepository repo) {
         this.repo = repo;
     }
 
+    /**
+     * GET request -> returns all persons in database
+     * @return List with persons in database
+     */
     @GetMapping("/persons")
     public List<Person> getAll() {
         return repo.findAll();
     }
 
+    /**
+     * GET request for specific person
+     * @param id Person ID
+     * @return badRequest if person does not exist, else ok
+     */
     @GetMapping("/person/{id}")
     public ResponseEntity<Person> getPersonById(@PathVariable Long id) {
         if (id < 0 || !repo.existsById(id)) {
@@ -30,6 +46,11 @@ public class PersonController {
         return ResponseEntity.ok(repo.findById(id).get());
     }
 
+    /**
+     * POST request to create person
+     * @param person Parsed from request body
+     * @return Ok response
+     */
     @PostMapping("/person/")
     public ResponseEntity<Person> addPerson(@RequestBody Person person) {
         Person personObj = repo.save(person);
@@ -37,10 +58,15 @@ public class PersonController {
         return ResponseEntity.ok(personObj);
     }
 
+    /**
+     * DELETE for deleting a person by ID
+     * @param id ID of person to delete
+     * @return badRequest if person not found, else ok
+     */
     @DeleteMapping("person/{id}")
     public ResponseEntity<Person> deletePerson(@PathVariable Long id) {
         Optional<Person> personToDelete = repo.findById(id);
-        if (!personToDelete.isPresent()) {
+        if (personToDelete.isEmpty()) {
             return ResponseEntity.badRequest().build();
         }
         repo.deleteById(id);
