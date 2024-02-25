2fa43039b766220ce708fdc8d9d4e94015837f17
Fayaz Wagid Hosain <f.r.wagidhosain@student.tudelft.nl>
2024-02-25 15:53:50 +0100

    Added person repository

---
+++ server/src/main/java/server/database/PersonRepository.java
@@ -0,0 +1,6 @@
+package server.database;
+
+import commons.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {}
