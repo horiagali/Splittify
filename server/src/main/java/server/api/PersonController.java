ac1b33c206a08a09251a8675b1fdbed4be3404ce
Fayaz Wagid Hosain <f.r.wagidhosain@student.tudelft.nl>
2024-02-25 16:55:09 +0100

    Removed unused import

--- server/src/main/java/server/api/PersonController.java
+++ server/src/main/java/server/api/PersonController.java
@@ -1,6 +1,5 @@
 package server.api;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import server.database.PersonRepository;
