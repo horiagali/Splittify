8b4d2cf8948cecfc15cf0193e3a2878738278cb0
Mihnea Nedelcu <m.nedelcu@student.tudelft.nl>
2024-03-17 23:38:45 +0100

    Added javadoc and fixed checkstyle rules

--- client/src/main/java/client/scenes/EditExpenseCtrl.java
+++ client/src/main/java/client/scenes/EditExpenseCtrl.java
@@ -50,6 +50,12 @@ public class EditExpenseCtrl implements Initializable {
     @FXML
     private TextField purposeTextField;
 
+    /**
+     * Constructor for EditExpenseCtrl.
+     * @param expense the expense in question.
+     * @param server the serverUtils.
+     * @param mainCtrl main controller.
+     */
     @Inject
     public EditExpenseCtrl(Expense expense, ServerUtils server, MainCtrl mainCtrl) {
         this.expense = expense;
@@ -58,43 +64,18 @@ public class EditExpenseCtrl implements Initializable {
         this.mainCtrl = mainCtrl;
     }
 
-    public void editExpense() {
-        expense.reverseSettleBalance();
-        expense.setTitle(purposeTextField.getText());
-        expense.setAmount(Double.parseDouble(amountTextField.getText()));
-        expense.setPayer(this.event.getParticipants().stream().filter(x -> x.getNickname().equals(nameChoiceBox.getValue())).findFirst().orElseThrow());
-        expense.setOwers(participantsVBox.getChildren().stream()
-                .filter(node -> node instanceof CheckBox)
-                .map(node -> (CheckBox) node)
-                .filter(CheckBox::isSelected)
-                .map(checkBox -> this.event.getParticipants().get(checkBoxes.indexOf(checkBox)))
-                .collect(Collectors.toList()));
-        expense.settleBalance();
-    }
-
-    @FXML
-    public void edit() {
-        try {
-            editButton.setOnAction(event -> editExpense());
-            //TODO: Edit in database.
-        }
-        catch (Exception e) {
-            messageLabel.setText("Please introduce some valid data!");
-        }
-    }
-
-    @FXML
-    void handleEquallyCheckbox(ActionEvent event) {
-        this.isSplitEqually = flipEquality(isSplitEqually);
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         List<Participant> participantNames = this.event.getParticipants();
         checkBoxes = new ArrayList<>();
 
-        this.nameChoiceBox.getItems().addAll(event.getParticipants().stream().map(Participant::getNickname).toList());
-        this.nameChoiceBox.getSelectionModel().select(this.event.getParticipants().indexOf(this.expense.getPayer()));
+        this.nameChoiceBox.getItems().addAll(
+                        event.getParticipants()
+                        .stream()
+                        .map(Participant::getNickname)
+                        .toList());
+        this.nameChoiceBox.getSelectionModel().select
+                (this.event.getParticipants().indexOf(this.expense.getPayer()));
         this.purposeTextField.setText(expense.getTitle());
         this.amountTextField.setText(Double.toString(expense.getAmount()));
         this.isSplitEqually = equallyCheckbox.isSelected();
@@ -115,18 +96,76 @@ public class EditExpenseCtrl implements Initializable {
         }
     }
 
+    /**
+     * Edits the expense instance without database modifications.
+     * Is called by edit which is set on action.
+     */
+    public void editExpense() {
+        expense.reverseSettleBalance();
+        expense.setTitle(purposeTextField.getText());
+        expense.setAmount(Double.parseDouble(amountTextField.getText()));
+        expense.setPayer(this.event.getParticipants()
+                .stream()
+                .filter(x -> x.getNickname().equals(nameChoiceBox.getValue()))
+                .findFirst()
+                .orElseThrow());
+        expense.setOwers(participantsVBox.getChildren().stream()
+                .filter(node -> node instanceof CheckBox)
+                .map(node -> (CheckBox) node)
+                .filter(CheckBox::isSelected)
+                .map(checkBox -> this.event.getParticipants().get(checkBoxes.indexOf(checkBox)))
+                .collect(Collectors.toList()));
+        expense.settleBalance();
+    }
+
+    /**
+     * Edits the expense object, as well as in storage.
+     */
+    @FXML
+    public void edit() {
+        try {
+            editButton.setOnAction(event -> editExpense());
+            //TODO: Edit in database backend.
+        }
+        catch (Exception e) {
+            messageLabel.setText("Invalid input, try again!");
+        }
+    }
+
+    /**
+     * Checkbox for equal split of the amount or not.
+     */
+    @FXML
+    void handleEquallyCheckbox() {
+        this.isSplitEqually = flipEquality(isSplitEqually);
+        //TODO: logic of balancing out without equality.
+    }
+
+
+    /**
+     * Returns to main overview.
+     */
     @FXML
     public void cancel() {
         mainCtrl.goToOverview();
     }
 
+    /**
+     * Logic for equal split button.
+     * @param value the boolean value to flip.
+     * @return the flipped value.
+     */
     public boolean flipEquality(boolean value) {
         return !value;
     }
 
+    /**
+     * Deletes an expense from an event and from the database.
+     * @param event the actionEvent of the button.
+     */
     @FXML
-    public void deleteExpense() {
-        event.getExpenses().remove(this.expense);
+    public void deleteExpense(ActionEvent event) {
+        this.event.getExpenses().remove(this.expense);
         //TODO: Delete from database.
     }
 }
