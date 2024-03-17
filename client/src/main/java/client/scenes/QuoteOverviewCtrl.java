453c9516ab3f4affa86bdd835aa47fadb133126b
Fayaz Wagid Hosain <f.r.wagidhosain@student.tudelft.nl>
2024-03-17 19:23:18 +0100

    Resolved conflict

--- client/src/main/java/client/scenes/QuoteOverviewCtrl.java
+++ client/src/main/java/client/scenes/QuoteOverviewCtrl.java
@@ -13,9 +13,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 
 public class QuoteOverviewCtrl implements Initializable {
