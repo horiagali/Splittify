d774f79b3627fbb1507d6e2ae793a6c46c88527f
Mihnea Nedelcu <m.nedelcu@student.tudelft.nl>
2024-03-17 23:26:38 +0100

    Finished most of implementation, still lacks backend

--- client/src/main/java/client/scenes/EditExpenseCtrl.java
+++ client/src/main/java/client/scenes/EditExpenseCtrl.java
@@ -11,16 +11,14 @@ import javafx.event.ActionEvent;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class EditExpenseCtrl implements Initializable {
 
@@ -28,65 +26,78 @@ public class EditExpenseCtrl implements Initializable {
     private Expense expense;
     private ServerUtils server;
     private MainCtrl mainCtrl;
-    List<CheckBox> checkBoxes;
+    private List<CheckBox> checkBoxes;
+    private boolean isSplitEqually;
+
     @FXML
     private TextField amountTextField;
-
+    @FXML
+    private Label messageLabel;
     @FXML
     private ComboBox<String> currencyComboBox;
-
     @FXML
     private Button delete;
-
     @FXML
-    private Button edit;
-
+    private Button cancel;
+    @FXML
+    private Button editButton;
     @FXML
     private CheckBox equallyCheckbox;
-
     @FXML
-    private TextField nameTextField;
-
+    private ChoiceBox<String> nameChoiceBox;
     @FXML
     private VBox participantsVBox;
-
     @FXML
     private TextField purposeTextField;
 
     @Inject
-    public EditExpenseCtrl(Expense expense, Event event, ServerUtils server, MainCtrl mainCtrl) {
+    public EditExpenseCtrl(Expense expense, ServerUtils server, MainCtrl mainCtrl) {
         this.expense = expense;
-        this.event = event;
+        this.event = expense.getEvent();
         this.server = server;
         this.mainCtrl = mainCtrl;
     }
 
-    @FXML
-    void addExpense(ActionEvent event) {
-
+    public void editExpense() {
+        expense.reverseSettleBalance();
+        expense.setTitle(purposeTextField.getText());
+        expense.setAmount(Double.parseDouble(amountTextField.getText()));
+        expense.setPayer(this.event.getParticipants().stream().filter(x -> x.getNickname().equals(nameChoiceBox.getValue())).findFirst().orElseThrow());
+        expense.setOwers(participantsVBox.getChildren().stream()
+                .filter(node -> node instanceof CheckBox)
+                .map(node -> (CheckBox) node)
+                .filter(CheckBox::isSelected)
+                .map(checkBox -> this.event.getParticipants().get(checkBoxes.indexOf(checkBox)))
+                .collect(Collectors.toList()));
+        expense.settleBalance();
     }
 
     @FXML
-    void back(ActionEvent event) {
+    public void edit() {
+        try {
+            editButton.setOnAction(event -> editExpense());
+            //TODO: Edit in database.
+        }
+        catch (Exception e) {
+            messageLabel.setText("Please introduce some valid data!");
+        }
     }
 
     @FXML
     void handleEquallyCheckbox(ActionEvent event) {
-
+        this.isSplitEqually = flipEquality(isSplitEqually);
     }
 
-    /**
-     * Initializes the controller.
-     * From Initializable
-     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         List<Participant> participantNames = this.event.getParticipants();
         checkBoxes = new ArrayList<>();
 
-        this.nameTextField.setText(expense.getPayer().getNickname());
+        this.nameChoiceBox.getItems().addAll(event.getParticipants().stream().map(Participant::getNickname).toList());
+        this.nameChoiceBox.getSelectionModel().select(this.event.getParticipants().indexOf(this.expense.getPayer()));
         this.purposeTextField.setText(expense.getTitle());
         this.amountTextField.setText(Double.toString(expense.getAmount()));
+        this.isSplitEqually = equallyCheckbox.isSelected();
         //TODO: add original currency to editor
         ObservableList<String> currencyList = FXCollections.observableArrayList(
                 "USD", "EUR", "GBP", "JPY");
@@ -104,4 +115,18 @@ public class EditExpenseCtrl implements Initializable {
         }
     }
 
+    @FXML
+    public void cancel() {
+        mainCtrl.goToOverview();
+    }
+
+    public boolean flipEquality(boolean value) {
+        return !value;
+    }
+
+    @FXML
+    public void deleteExpense() {
+        event.getExpenses().remove(this.expense);
+        //TODO: Delete from database.
+    }
 }
