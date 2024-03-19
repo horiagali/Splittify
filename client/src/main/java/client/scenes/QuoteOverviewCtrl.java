1d5d51c885f18392dde79207137c4593ab98a3a4
Martijn van Leest <m.j.c.vanleest@student.tudelft.nl>
2024-03-19 13:41:56 +0100

    checkstyle error

--- client/src/main/java/client/scenes/QuoteOverviewCtrl.java
+++ client/src/main/java/client/scenes/QuoteOverviewCtrl.java
@@ -15,7 +15,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.RadioMenuItem;
