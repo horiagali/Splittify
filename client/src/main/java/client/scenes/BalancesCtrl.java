b4645c392ec9980dd11f92a9cb84b74bd36c4641
Iulia Slănină <i.slanina-1@student.tudelft.nl>
2024-03-23 13:55:13 +0100

    checkstyle

--- client/src/main/java/client/scenes/BalancesCtrl.java
+++ client/src/main/java/client/scenes/BalancesCtrl.java
@@ -8,7 +8,6 @@ import commons.Participant;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
