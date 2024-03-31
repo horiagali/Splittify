cac32177d6b4495358b439cc8d50e0d465ee9e20
Iulia Slănină <i.slanina-1@student.tudelft.nl>
2024-03-31 02:13:57 +0200

    added marked recieved button

--- client/src/main/java/client/scenes/SettleDebtsCtrl.java
+++ client/src/main/java/client/scenes/SettleDebtsCtrl.java
@@ -12,8 +12,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-
-import java.awt.*;
+import javafx.scene.control.Button;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -26,9 +25,11 @@ public class SettleDebtsCtrl implements Initializable {
     @FXML
     private TableView<Expense> tableView;
     @FXML
-    private TableColumn<Expense, Button> actionColumn;
+    private TableColumn<Expense, Void> actionColumn;
     @FXML
     private TableColumn<Expense, String> debtColumn;
+    @FXML
+    private TableColumn<Expense, Void> reminderColumn;
     @FXML
     private ObservableList<Expense> data;
     /**
@@ -57,16 +58,35 @@ public class SettleDebtsCtrl implements Initializable {
         this.event = event;
     }
 
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-            actionColumn.setCellFactory(col -> new TableCell<Expense, Button>() {
-                @FXML
-                private final Button actionButton = new Button("Mark Received");
-            });
-            debtColumn.setCellValueFactory(q ->
-                    new SimpleStringProperty(q.getValue().getPayer().getNickname() + " gives " +
-                            q.getValue().getAmount() + " to " +
-                            q.getValue().getOwers().get(0).getNickname()));
+        debtColumn.setCellValueFactory(q ->
+                new SimpleStringProperty(q.getValue().getPayer().getNickname() + " gives " +
+                        q.getValue().getAmount() + " to " +
+                        q.getValue().getOwers().get(0).getNickname()));
+        actionColumn.setCellFactory(col -> new TableCell<Expense, Void>() {
+            private final Button actionButton = new Button("Mark Received");
+
+            {
+                actionButton.setOnAction(event -> {
+                    Expense currentExpense = getTableView().getItems().get(getIndex());
+                    getTableView().getItems().remove(currentExpense);
+                });
+            }
+
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    setGraphic(actionButton);
+                }
+            }
+        });
+
+        tableView.getColumns().add(actionColumn);
     }
     /**
      * refresh function
