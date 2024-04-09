63c9290a06224344e9b9b8661a24a4abbe3d8498
Mihnea Nedelcu <m.nedelcu@student.tudelft.nl>
2024-04-09 03:24:10 +0200

    Created admin password generation serverside

---
+++ server/src/main/java/server/service/PasswordService.java
@@ -0,0 +1,23 @@
+package server.service;
+
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Service
+public class PasswordService {
+
+    /**
+     * Password generator.
+     * @return password;
+     */
+    public static String generatePassword() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] randomBytes = new byte[8];
+        secureRandom.nextBytes(randomBytes);
+        String password = Base64.getUrlEncoder().withoutPadding().encodeToString(randomBytes);
+        System.out.println(password);
+        return password;
+    }
+}
