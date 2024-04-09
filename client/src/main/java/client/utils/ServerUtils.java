ba65fdd6b8aaa5d057d5d49a9d16d998ec99aa1a
Mihnea Nedelcu <m.nedelcu@student.tudelft.nl>
2024-04-09 03:31:17 +0200

    Fixed pipeline

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -21,7 +21,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
-import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
