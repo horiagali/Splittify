6bda6353fa2f63b9c4763d247298a3234c5c5f6c
Iulia Slănină <i.slanina-1@student.tudelft.nl>
2024-04-14 15:03:58 +0200

    more tests

--- client/src/test/java/client/utils/CurrencyTest.java
+++ client/src/test/java/client/utils/CurrencyTest.java
@@ -2,18 +2,29 @@ package client.utils;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Map;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class CurrencyTest {
 
     @Test
     void round() {
-        double x = 1;
-        assertEquals(1.0, Currency.round(x));
+        Double x = 1.0;
+        Double y = Currency.round(x);
+        assertTrue(Double.compare(y, x) == 0);
+    }
+
+    @Test
+    void round2() {
+        double x = 1.1;
+        Double y = Currency.round(x);
+        assertTrue(Double.compare(y, x) == 0);
+    }
+
+    @Test
+    void round3() {
+        double x = 1.2;
+        Double y = Currency.round(x);
+        assertTrue(Double.compare(y, x) == 0);
     }
 
     @Test
@@ -21,16 +32,42 @@ class CurrencyTest {
         Currency.setCurrencyUsed("EUR");
         assertEquals("EUR", Currency.getCurrencyUsed());
     }
-
     @Test
-    void getCurrencies() {
-        Map<String, Double> mp = Currency.getCurrencies("EUR", LocalDate.now());
-        assertNotNull(mp);
+    void getCurrencyUsed1() {
+        Currency.setCurrencyUsed("CHF");
+        assertEquals("CHF", Currency.getCurrencyUsed());
     }
+    @Test
+    void getCurrencyUsed2() {
+        Currency.setCurrencyUsed("USD");
+        assertEquals("USD", Currency.getCurrencyUsed());
+    }
+    @Test
+    void getCurrencyUsed3() {
+        Currency.setCurrencyUsed("RON");
+        assertEquals("RON", Currency.getCurrencyUsed());
+    }
+
 
     @Test
     void setCurrencyUsed() {
         Currency.setCurrencyUsed("EUR");
         assertEquals("EUR", Currency.getCurrencyUsed());
     }
+    @Test
+    void setCurrencyUsed2() {
+        Currency.setCurrencyUsed("USD");
+        assertEquals("USD", Currency.getCurrencyUsed());
+    }
+    @Test
+    void setCurrencyUsed3() {
+        Currency.setCurrencyUsed("CHF");
+        assertEquals("CHF", Currency.getCurrencyUsed());
+    }
+    @Test
+    void setCurrencyUsed4() {
+        Currency.setCurrencyUsed("RON");
+        assertEquals("RON", Currency.getCurrencyUsed());
+    }
+
 }
